e9623f9fd5bfe551ca85dedcbc421c32828327c7
PeterYamo <pmusayamo@gmail.com>
2023-03-21 11:31:27 -0700

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
+/**
+ * print_alphabet - Make the alphabet
+ * Return: void
+ */
 
- /**
-  * print_alphabet - Make the alphabet
-  * Return: void
-  */
 
-
- void print_alphabet(void)
+void print_alphabet(void)
 
 {
 
